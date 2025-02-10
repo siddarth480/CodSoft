df33090c94c25d890e06241bd7ff2998ec161226
siddarth480 <123230469+siddarth480@users.noreply.github.com>
2025-02-10 09:52:54 +0530

    NumberGuessingGame.java

---
+++ NumberGuessingGame.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+        int roundsWon = 0;
+        
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int attempts = 0;
+            int maxAttempts = 10;
+            boolean guessedCorrectly = false;
+            
+            System.out.println("\nWelcome to the Number Guessing Game!");
+            System.out.println("Guess a number between 1 and 100. You have " + maxAttempts + " attempts.");
+            
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+                
+                if (userGuess < numberToGuess) {
+                    System.out.println("Too low! Try again.");
+                } else if (userGuess > numberToGuess) {
+                    System.out.println("Too high! Try again.");
+                } else {
+                    System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                    roundsWon++;
+                    break;
+                }
+            }
+            
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The correct number was: " + numberToGuess);
+            }
+            
+            System.out.println("Rounds won: " + roundsWon);
+            System.out.print("Do you want to play again? (yes/no): ");
+            String response = scanner.next().toLowerCase();
+            playAgain = response.equals("yes");
+        }
+        
+        System.out.println("Thanks for playing! Your total rounds won: " + roundsWon);
+        scanner.close();
+    }
+}
